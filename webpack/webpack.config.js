ddc6cc56b7c14c5170d817a15a61fac8682dc550
rhodri.hewitson <rhodri.hewitson@nagra.com>
2018-03-22 15:53:48 +0000

    Change build so we don't exclude everything in node_modules during webpack. This means wee can locally build the npm install module

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,24 @@
 const moment = require('moment');
 const pkg = require('../package.json');
 const webpack = require('webpack');
+const path = require('path');
+
+function excludeNodeModulesExcept (modules)
+{
+    var pathSep = path.sep;
+    if (pathSep == '\\') // must be quoted for use in a regexp:
+        pathSep = '\\\\';
+    var moduleRegExps = modules.map (function (modName) { return new RegExp("node_modules" + pathSep + modName)})
+
+    return function (modulePath) {
+        if (/node_modules/.test(modulePath)) {
+            for (var i = 0; i < moduleRegExps.length; i ++)
+                if (moduleRegExps[i].test(modulePath)) return false;
+            return true;
+        }
+        return false;
+    };
+}
 
 module.exports = {
   entry: {
@@ -21,7 +39,7 @@ module.exports = {
     }],
 
     loaders: [{
-      exclude: /node_modules/,
+      exclude: excludeNodeModulesExcept(["videojs-fairplay"]),
       loader: 'babel',
       test: /\.js$/,
 
